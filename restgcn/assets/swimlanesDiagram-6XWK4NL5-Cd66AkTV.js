80fd8511bf89ab68772cdcff8a3e02151ba527f9
davidots429 <davidots429@gmail.com>
2026-09-20 22:36:56 +0900

    fix randering

---
+++ restgcn/assets/swimlanesDiagram-6XWK4NL5-Cd66AkTV.js
@@ -0,0 +1,8 @@
+import"./Mermaid-CZzsUId0.js";import{r as e}from"./chunk-PTVI3W5X-DVfdoxm6.js";import"./chunk-X5WJYYCX-DStMnrYo.js";import"./chunk-AHS5MEEA-C4Q_NLg-.js";import"./chunk-PLCLPJVV-BfMyJHbe.js";import"./chunk-ECMXQ4X3-B1LEQyUn.js";import"./chunk-AYHZ2ZZ4-4CyZ2RDn.js";import"./chunk-IPM4HZQ6-CQmHNgJe.js";import"./chunk-TYR5776D-DGkax0Eo.js";import"./chunk-LBQO3HYP-CsAgv7bJ.js";import"./chunk-BULPQSRM-C5LeUka0.js";import"./chunk-VOXL5ABF-BSN4irnd.js";import"./chunk-R2G3K5KZ-ORU4cGVC.js";import"./chunk-YD6RSKXN-C6j1AAnl.js";import"./chunk-YJHQES7P-mvPKceSS.js";import"./chunk-Z7BGCTMV-BpZuaIbp.js";import"./chunk-CXMX27UD-VR6hGkZP.js";import{r as t,t as n}from"./chunk-6HLVECFW-B-byuzYk.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
